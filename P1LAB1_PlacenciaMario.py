0f3577ca75cffed75d2c82eb7e41e169b19a37b3
MarioPlacencia15 <placencm6353@student.faytechcc.edu>
2025-02-10 21:44:42 -0600

    Add files via upload
    First submission

---
+++ P1LAB1_PlacenciaMario.py
@@ -0,0 +1,7 @@
+ # Mario Placencia
+ # 2025-02-15
+ # P1LAB1
+ # This program prompts users for first and last name then displays welcome message
+first_name = input('Enter your first name: ')
+last_name = input('Enter your last name: ')
+print('Hello,', first_name, last_name + '! Welcome to CTI-110')
