2eb3776f03c163e30159e5d258204d4b1be1cdc1
MarioPlacencia15 <placencm6353@student.faytechcc.edu>
2025-02-20 19:18:26 -0600

    Add files via upload
    uploaded

---
+++ P1HW1_PlacenciaMario.py
@@ -0,0 +1,13 @@
+# Mario Placencia 
+# 02/27/2025
+# P1HW1
+# A brief description of the project
+
+print('-----Calculating Exponenets-----')
+
+
+base = int(input("Enter an integer as the base value: "))
+exponent = int(input("Enter an integer as the exponent: "))
+result =  base **  exponent
+print ( {base} ,'raised to the power of', {exponent} 'is', {result} '!!')
+
